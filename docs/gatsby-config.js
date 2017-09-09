33f38610b66640ff72f3a7d927080ef1c65d83bd
Dylan Mozlowski <dylan.mozlowski@gmail.com>
2017-09-09 12:27:02 -0700

    Update gatsby-config.js

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -2,6 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `<repo>`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-105846113-1',
+      },
+    }
+  ],
   pathPrefix: '/<repo>'
 }
